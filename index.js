e0f47dc4fa85f56ba4ecadf475d2f87ca55408db
Matt Wilcox <nanpx@users.noreply.github.com>
2017-11-01 18:58:47 -0600

    feat: Switched to D-Bus binding instead of nmcli
    Updates user experience to match more closely to Ubuntu NetworkManager.
    
    BREAKING CHANGE: API and dependency has changed.
    
    Fixes AIST-7
    Fixes AIST-8
    Fixes AIST-17
    Fixes
    AIST-18
    Fixes AIST-20

--- index.js
+++ index.js
@@ -1,71 +1,48 @@
-/** ***********************************************************************
- * COPYRIGHT (c) 2017 LGS INNOVATIONS - ALL RIGHTS RESERVED
- * LGS INNOVATIONS PROPRIETARY - USE PURSUANT TO COMPANY INSTRUCTIONS
- *************************************************************************/
+/**
+Copyright 2017 LGS Innovations
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+
+const DeviceManager = require('./lib/devices/device-manager');
+const ConnectionManager = require('./lib/connections/connection-manager');
+const nm = require('dbus-network-manager');
 
 (() => {
   'use strict';
-  const UtilChildPromise = global.utils.UtilChildProcess;
-  const NMCLI_CMD = 'nmcli';
-  const NMCLI_ARGS = ['-v'];
-
-  // BITS libs
-  const BondingManager = require('./server/bonding/bonding-manager');
-  const EthernetDeviceManager = require('./server/ethernet/ethernet-device-manager');
-  const WifiDeviceManager = require('./server/wifi/wifi-device-manager');
-  const CellularDeviceManager = require('./server/cellular/cellular-device-manager');
-  const NmcliManager = require('./server/nmcli/nmcli-manager');
-  const BridgeManager = require('./server/bridge/bridge-device-manager');
-  const RadioManager = require('./server/radio/radio-manager');
 
   class NetworkManagerApp {
     constructor() {
-      this._nmcliManager = new NmcliManager();
-      this._wifiDeviceManager = new WifiDeviceManager(this._nmcliManager);
-      this._ethernetDeviceManager = new EthernetDeviceManager(this._nmcliManager);
-      this._cellularDeviceManager = new CellularDeviceManager(this._nmcliManager);
-      this._bondingManager = new BondingManager(this._nmcliManager);
-      this._bridgeManager = new BridgeManager(this._nmcliManager);
-      this._radioManager = new RadioManager();
+      this._deviceManager = new DeviceManager();
+      this._connectionManager = new ConnectionManager();
     }
 
     load(messageCenter) {
-      return UtilChildPromise.createSpawnPromise(NMCLI_CMD, NMCLI_ARGS)
-      .then(result => {
-        const test = result.stdout[0].replace(/(\r\n|\n|\r)/gm, '');
-        const ver = test.match(/^.*version (\d).(\d.)(\d).*$/);
-        if ('0' === ver[1]) {
-          return Promise.reject(new Error('Unsupported Hardware/Configuration - Incorrect NMCLI version'));
-        }
-      })
-      .catch(err => {
-        // If there is no NMCLI, then we should bail as we can never hope to work
-        if ('ENOENT' !== err.code) {
-          throw err;
-        }
-        return Promise.reject(new Error('Unsupported Hardware/Configuration - Incorrect NMCLI version'));
-      })
-      .then(() => this._nmcliManager.load())
-      .then(() => this._wifiDeviceManager.load(messageCenter))
-      .then(() => this._ethernetDeviceManager.load(messageCenter))
-      .then(() => this._cellularDeviceManager.load(messageCenter))
-      .then(() => this._bridgeManager.load(messageCenter))
-      .then(() => this._bondingManager.load(messageCenter))
-      .then(() => this._radioManager.load(messageCenter));
-    }
-
-    unload() {
       return Promise.resolve()
-        .then(() => this._radioManager.unload())
-        .then(() => this._bridgeManager.unload())
-        .then(() => this._bondingManager.unload())
-        .then(() => this._cellularDeviceManager.unload())
-        .then(() => this._ethernetDeviceManager.unload())
-        .then(() => this._wifiDeviceManager.unload())
-        .then(() => this._nmcliManager.unload());
+      .then(() => nm.connect())
+      .then((connectedNetworkManager) => {
+        return Promise.resolve()
+        .then(() => this._deviceManager.load(messageCenter, connectedNetworkManager))
+        .then(() => this._connectionManager.load(messageCenter, connectedNetworkManager));
+      });
     }
 
-  } // End Class
+    unload(messageCenter) {
+      return Promise.resolve()
+      .then(() => this._connectionManager.unload(messageCenter))
+      .then(() => this._deviceManager.unload(messageCenter));
+    }
+  }
 
   module.exports = new NetworkManagerApp();
 })();
