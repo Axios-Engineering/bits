edd5c278889a6bc38183dea06708d4b70f796c3a
jewalt <jewalt@lgsinnovations.com>
2017-11-09 18:20:34 -0700

    fix: do not load unsupported modules
    Network-Manager depends on a version of Linux networkManager being installed.  Do not load the
    module if that version does not exist.
    
    closes BITS-63

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ const nm = require('dbus-network-manager');
 (() => {
   'use strict';
 
+  const UtilChildPromise = global.utils.UtilChildProcess;
+  const NMCLI_CMD = 'nmcli';
+  const NMCLI_ARGS = ['-v'];
+
   class NetworkManagerApp {
     constructor() {
       this._deviceManager = new DeviceManager();
@@ -28,7 +32,21 @@ const nm = require('dbus-network-manager');
     }
 
     load(messageCenter) {
-      return Promise.resolve()
+      return UtilChildPromise.createSpawnPromise(NMCLI_CMD, NMCLI_ARGS)
+      .then((result) => {
+        const test = result.stdout[0].replace(/(\r\n|\n|\r)/gm, '');
+        const ver = test.match(/^.*version (\d).(\d.)(\d).*$/);
+        if ('0' === ver[1]) {
+          return Promise.reject(new Error('Unsupported Hardware/Configuration - Module not supported on this platform'));
+        }
+      })
+      .catch((err) => {
+        // If there is no NMCLI, then we should bail as we can never hope to work
+        if ('ENOENT' !== err.code) {
+          throw err;
+        }
+        return Promise.reject(new Error('Unsupported Hardware/Configuration - Module not supported on this platform'));
+      })
       .then(() => nm.connect())
       .then((connectedNetworkManager) => {
         return Promise.resolve()
